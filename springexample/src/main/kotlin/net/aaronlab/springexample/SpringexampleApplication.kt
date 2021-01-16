d97f6244ca55b3f86dc65e372a06bd81bdf080b7
Aaron Lee <aaronlab.net@gmail.com>
2021-01-16 18:30:07 +0900

    Init project

---
+++ springexample/src/main/kotlin/net/aaronlab/springexample/SpringexampleApplication.kt
@@ -0,0 +1,11 @@
+package net.aaronlab.springexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringexampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringexampleApplication>(*args)
+}
