6d334c3281e4f38902e6a54b729ca6b118476a04
Aaron Lee <aaronlab.net@gmail.com>
2021-01-18 22:24:42 +0900

    How to make GET controllers

---
+++ first_request_exercise/src/main/kotlin/net/aaronlab/first_request_exercise/controllers/get/GetController.kt
@@ -0,0 +1,51 @@
+package net.aaronlab.first_request_exercise.controllers.get
+
+import net.aaronlab.first_request_exercise.models.UserRequest
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping(path = ["/api/get"])
+class GetController {
+    /*
+    기본 GET
+     */
+    @GetMapping(path = ["/hello"])
+    fun hello(): String {
+        return "hello"
+    }
+
+    @GetMapping(path = ["/path-variables/{name}"])
+    fun pathVariables(@PathVariable name: String): String {
+        return name
+    }
+
+    @GetMapping(path = ["/path-variables2/{name}"])
+    fun pathVariables2(@PathVariable(value = "name") _name: String): String {
+        val name = "aaron"
+        return _name
+    }
+
+    @GetMapping(path = ["/request-param"])
+    fun requestParam(@RequestParam name: String, @RequestParam(value = "age") _age: Int): String {
+        val age = 20
+        return name + age
+    }
+
+    /*
+    하이픈 불가
+     */
+    @GetMapping(path = ["/request-param/object"])
+    fun requestParamWithObj(userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+    }
+
+    @GetMapping(path = ["/request-param/map"])
+    fun requestParamWithMap(@RequestParam map: Map<String, Any>): Map<String, Any> {
+        println(map)
+        val phoneNumber = map["phone-number"]
+        println(phoneNumber)
+        return map
+    }
+
+}
---
+++ first_request_exercise/src/main/kotlin/net/aaronlab/first_request_exercise/models/UserRequest.kt
@@ -0,0 +1,6 @@
+package net.aaronlab.first_request_exercise.models
+
+data class UserRequest(
+    var name: String? = null,
+    var age: Int? = null
+)
